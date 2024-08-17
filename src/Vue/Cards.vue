8910bc2276af10eae157c7a47594732cd584086a
unit-404 <conway24b@gmail.com>
2024-08-17 10:52:48 +0700

    new elements

--- src/Vue/Cards.vue
+++ src/Vue/Cards.vue
@@ -9,14 +9,20 @@
 
         <div class="card">
             <h6>Featured Project</h6>
+
+            <div>Помогите, печень садится</div>
         </div>
 
         <div class="card">
             <h6>Most Detailed</h6>
+
+            <div>Ай, печень садится</div>
         </div>
 
         <div class="card">
             <h6>Last Standards</h6>
+
+            <div>Печёнка садится</div>
         </div>
 
     </div>
