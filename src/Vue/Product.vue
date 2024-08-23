6ee94ac0820763785da4ca6774909ad48b9e41f5
unit-404 <conway24b@gmail.com>
2024-08-23 22:04:26 +0700

    grid-based units

--- src/Vue/Product.vue
+++ src/Vue/Product.vue
@@ -5,7 +5,37 @@
 <template>
     <section class="products" data-transparent>
 
-        <article id="idc-l2" data-scheme="solid" data-highlight="3">
+        <article id="unite-js" data-scheme="accent" data-highlight="1">
+            <h3><LucideIcon name="medal"/> Unite.JS <LucideIcon name="medal"/></h3>
+
+            <div class="subscript"><a href="#">Learn More</a></div>
+            <div class="description">Our unified JS library package.</div>
+
+            <div class="space"></div>
+            <div class="description align-start">
+                <h6>Features:</h6>
+                <ul>
+                    <li>Grid utils (used in application).</li>
+                    <li>Reactive and objects utils.</li>
+                    <li>Pointer and interaction utils.</li>
+                    <li>Dynamic styles workers.</li>
+                    <li>Web Worker core utils.</li>
+                    <li>DOM utils, observers.</li>
+                    <li>Cryptography utils.</li>
+                </ul>
+            </div>
+
+            <div class="space"></div>
+
+            <div class="buttons">
+                <a href="#"><button data-scheme="inverse" type="button" data-highlight-hover="1"><LucideIcon name="book-marked"/>Documentation<LucideIcon name="book-marked"/></button></a>
+                <a href="https://github.com/BZ-0/unite.js"><button data-scheme="inverse" type="button" data-highlight-hover="1"><LucideIcon name="github"/>Source Code<LucideIcon name="github"/></button></a>
+            </div>
+        </article>
+
+        <div class="word"><LucideIcon name="circle-fading-plus"/></div>
+
+        <article id="idc-l2" data-scheme="solid" data-highlight="4">
             <h3><LucideIcon name="crown"/> IDC-L2 <LucideIcon name="crown"/></h3>
 
             <div class="subscript"><a href="#">Learn More</a></div>
@@ -32,7 +62,7 @@
 
         <div class="word"><LucideIcon name="circle-fading-plus"/></div>
 
-        <article id="idc-cloud" data-scheme="solid" data-highlight="3">
+        <article id="idc-cloud" data-scheme="solid" data-highlight="4">
             <h3><LucideIcon name="server-cog"/> IDC-Cloud <LucideIcon name="server-cog"/></h3>
 
             <div class="subscript"><a href="#">Learn More</a></div>
